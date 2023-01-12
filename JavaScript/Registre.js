6b7b4562a39375ec5508a0398add3a28c05b9684
kerlin2024 <113436926+kerlin2024@users.noreply.github.com>
2023-01-12 09:56:19 +0100

    Mes avanzament

--- JavaScript/Registre.js
+++ JavaScript/Registre.js
@@ -1,1 +1,20 @@
+//Crear inicio de sessio de la pagina web
+function CrearSessio(){
+ <script>
 
+document.addEventListener('DOMContentLoaded', function () {(function () {
+
+
+    const sessionstore = sessionStorage;
+    const usuario = document.getElementById("name");
+    const password = document.getElementById("password");
+
+    function Register(){
+        alert("ALERT!");
+        sessionstore.setItem("usuario", usuario.value);
+        sessionstore.setItem("contrasenya", password.value);
+        alert("Usuario registrado");
+    }
+})  });
+</script>
+}
